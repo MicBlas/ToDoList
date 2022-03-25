20f9299ef6a1f757068002be9b19e55a5baef406
micblas <Micblas@wp.pl>
2022-03-25 15:18:37 +0100

    fix date, upgrate deleted tasks

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const addTask = document.querySelector("button");
 let containerTasks = document.querySelector(".containerTasks");
+let task = document.querySelector("input.newTask");
 
+//Add new task
 addTask.addEventListener("click",function(){
-    let task = document.querySelector("input.newTask");
-
     if(task.value !== ""){
         let newCheckbox = document.createElement("input");
         newCheckbox.type="checkbox";
@@ -12,13 +12,15 @@ addTask.addEventListener("click",function(){
         let newInput = document.createElement("input");
         newInput.classList.add("newInputStyle");
 
-        const parentOfTasks = document.querySelector(".containerTasks");
-        parentOfTasks.appendChild(newCheckbox);
-        parentOfTasks.appendChild(newInput);
+        containerTasks.appendChild(newCheckbox);
+        containerTasks.appendChild(newInput);
 
         newInput.value=task.value;
+        
+        //saveTask();
+        newInput.classList.add("dataset");
+        newInput.dataset.task =task.value;
         task.value ="";
-
     }else{
         alert("Write your new task");
     }
@@ -28,31 +30,31 @@ addTask.addEventListener("click",function(){
 containerTasks.addEventListener("change",function(e){
     let nextElement = e.target.nextElementSibling;
     
-    if(e.target.classList.contains("done")){
-        //console.log("done");
-        
-        nextElement.classList.toggle("deleteTask");
-        e.target.classList.toggle("deleteTask");
-    
-        //Deleted task goes to the end of the list
-    if(e.target.classList.contains("deleteTask")){
-        //console.log("works");
-        containerTasks.appendChild(nextElement);
-        containerTasks.insertBefore(e.target,nextElement);
-    }else{
-        nextElement.dataset.time= "";
-        //nextElement.value =
-    }
-    
     //Add date and time
     let today = new Date();
     let date = today.getFullYear()+"."+today.getMonth()+"."+today.getDay();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     let timeOfTask = date+" "+time;
     //alert(timeOfTask);
-   
+    
     nextElement.classList.add("dataset");
     nextElement.dataset.time= timeOfTask;
     //alert(nextElement.dataset.time);
-    nextElement.value +="   time:"+timeOfTask;
-}})
+    
+    
+    
+    if(e.target.classList.contains("done")){
+        nextElement.classList.toggle("deleteTask");
+        e.target.classList.toggle("deleteTask");
+    
+        //Deleted task goes to the end of the list
+    if(e.target.classList.contains("deleteTask")){
+        containerTasks.appendChild(nextElement);
+        containerTasks.insertBefore(e.target,nextElement);
+        nextElement.value +="   time:"+timeOfTask;
+    }else{
+        nextElement.dataset.time = "";
+        nextElement.value = nextElement.dataset.task;
+    }
+}
+})
